2ab795d97019cc5c36abfde2f78202d42bed7ac8
alexl <alexandermlugbiihl@gmail.com>
2023-02-16 16:46:02 -0500

    Add User logout backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -24,7 +24,11 @@ router.post('/', async (req, res, next) => {
 }
 );
 
-
+router.delete('/', (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+}
+);
 
 
 
