e53c504766e0ff004d0bf96df8f665bd56d89641
alexl <alexandermlugbiihl@gmail.com>
2023-02-20 13:24:50 -0500

    previous associations

---
+++ backend/db/models/review.js
@@ -0,0 +1,36 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Review extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Review.hasMany(models.ReviewImage, { foreignKey: "reviewId" })
+    }
+  }
+  Review.init({
+    spotId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    review: {
+      type: DataTypes.STRING
+    },
+    stars: {
+      type: DataTypes.INTEGER,
+    },
+  }, {
+    sequelize,
+    modelName: 'Review',
+  });
+  return Review;
+};
--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -43,7 +43,7 @@ module.exports = (sequelize, DataTypes) => {
     }
 
     static associate(models) {
-      // define association here
+      User.hasMany(models.Spot, { foreignKey: "ownerId" })
     }
   };
 
