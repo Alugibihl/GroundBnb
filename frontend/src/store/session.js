6b4c133f92733b49cd16fdd5dba6b84300245276
alexl <alexandermlugbiihl@gmail.com>
2023-03-10 13:30:12 -0500

    initialize sign up page for non signed in user

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -36,6 +36,22 @@ export const login = (user) => async (dispatch) => {
     dispatch(setUser(data.user));
     return response;
 };
+export const signup = (user) => async (dispatch) => {
+    const { username, firstName, lastName, email, password } = user;
+    const response = await csrfFetch("/api/users", {
+        method: "POST",
+        body: JSON.stringify({
+            username,
+            firstName,
+            lastName,
+            email,
+            password,
+        }),
+    });
+    const data = await response.json();
+    dispatch(setUser(data.user));
+    return response;
+};
 
 const initialState = { user: null };
 
