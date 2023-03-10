dd7ec7d476a09ccebfad872d09fd66cd00869854
alexl <alexandermlugbiihl@gmail.com>
2023-03-10 16:56:23 -0500

    refactor logic in profile button

--- frontend/src/components/SignupFormModal/index.js
+++ frontend/src/components/SignupFormModal/index.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import { useModal } from "../../context/Modal";
 import * as sessionActions from "../../store/session";
-import './SignupForm.css';
+import './SignupFormModal.css';
 
 function SignupFormModal() {
     const dispatch = useDispatch();
