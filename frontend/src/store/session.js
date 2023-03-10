20232b7a2573c9fac166b678230fe2b8f3d481df
alexl <alexandermlugbiihl@gmail.com>
2023-03-10 14:05:54 -0500

    navlink

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -36,6 +36,13 @@ export const login = (user) => async (dispatch) => {
     dispatch(setUser(data.user));
     return response;
 };
+export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+        method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+};
 export const signup = (user) => async (dispatch) => {
     const { username, firstName, lastName, email, password } = user;
     const response = await csrfFetch("/api/users", {
